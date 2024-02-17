ce5f57e092f049518a0fa83102057698110190a3
githunb yuki918 <tada950918@gmail.com>
2024-02-17 17:15:30 +0900

    inertiaとvueの調整

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    // server: {
+    //     host: true
+    // },
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
